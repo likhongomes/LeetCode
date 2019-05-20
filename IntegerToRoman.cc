5b7b5fbd622531b517c063777f7d605939fafdbb
Likhon Leo Gomes <tug60499@temple.edu>
2019-05-20 11:34:10 -0400

    Laying the foundation
    init

---
+++ IntegerToRoman.cc
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        
+        int temp = num;
+        
+        if(num=<1000){
+            
+        } else if(num=<500){
+            
+        } else if(num=<100){
+            
+        } else if(num=<50){
+            
+        } else if(num=<10){
+            
+        } else if(num=<5){
+            
+        }else{
+            
+        }
+            
+        
+            
+        
+        
+        
+        
+    }
+};
